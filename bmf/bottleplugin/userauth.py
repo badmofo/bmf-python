b57d69dc7bbd003acea38d821dd62a048952277e
Lucas Ryan <badmofo@gmail.com>
2016-05-11 20:38:14 -0700

    fixed userauth bottle plugin (for the most part)

--- bmf/bottleplugin/userauth.py
+++ bmf/bottleplugin/userauth.py
@@ -1,41 +1,104 @@
 #!/usr/bin/python
 
 '''
-WARNING: Don't use this yet.  Not done/tested yet + doesn't implement XSRF mechanisms yet.
+WARNING: Don't use this yet.  Not fully tested yet.
 '''
 
 import inspect
 import bottle
 import os
 import datetime
+import hmac
+import hashlib
+import scrypt
+import random
+import re
+import time
 
-class BaseSessionStorage(object):
-    def __init__(self, prefix, redis, get_user_by_user_id):
-        self.prefix = prefix
-        self.redis = redis
-        self.get_user_by_user_id = get_user_by_user_id
+
+def now():
+    return int(time.time())
+
+class SqlSessionStorage:
+    '''
+    CREATE TABLE session(
+        id CHAR(32),
+        user_id INT NOT NULL,
+        expiry BIGINT UNSIGNED NOT NULL,
+
+        KEY(user_id, expiry),
+        PRIMARY KEY(id)
+    );
+    '''
+    def __init__(self, db):
+        self.db = db
+        
+    def store(self, session_id, user_id, session_lifetime):
+        self.db.replace('session', id=session_id, user_id=user_id, expiry=now() + session_lifetime)
     
-    def store(self, session_id, user_id, session_lifetime_ms):
-        key = self.prefix + session_id
-        self.redis.set(key, user_id)
-        self.redis.pexpire(key, session_lifetime_ms)
+    def get_user_id(self, session_id):
+        session = self.db.get('select * from session where id=%s and expiry > %s', session_id, now())
+        if session:
+            return session.user_id
     
-    def get_user(self, session_id, session_lifetime_ms=None):
-        if session_lifetime_ms:
-            self.redis.pexpire(session_id, session_lifetime_ms)
-        user_id = self.redis.get(session_id)
-        if user_id is not None:
-            return self.get_user_by_user_id(user_id)            
+    def refresh(self, session_id, session_lifetime):
+        self.db.execute('update session set expiry=%s where id=%s', now() + session_lifetime, session_id)
     
     def expire(self, session_id):
-        self.redis.delete(session_id)
+        self.db.execute('delete from session where expiry < %s or id=%s', now(), session_id)
+        
+    def expire_by_user(self, user_id, excluded_session_id=''):
+        self.db.execute('delete from session where user_id=%s and id<>%s', user_id, excluded_session_id)
+
+
+class NullLockoutManager(object):
+    def clear_lockouts(self, user_id):
+        pass
     
-    def expire_by_user(self, user_id, excluded_session_id=None):
-        # TODO: make more efficient
-        for key in self.redis.keys(self.prefix + '*'):
-            session_id = key[len(self.prefix):]
-            if excluded_session_id != session_id and self.redis.get(key) == user_id:
-                self.expire(session_id)
+    def get_lockouts(self, user_id):
+        return []
+    
+    def is_locked_out(self, ip, user_id):
+        pass
+    
+    def authentication_attempt(self, ip, user_id, success):
+        pass
+
+
+class RedisLockoutManager(object):
+    def __init__(self, redis, lockout_threshold=30, lockout_duration_sec=60*60):
+        self.redis = redis
+        self.lockout_threshold = lockout_threshold
+        self.lockout_duration_sec = lockout_duration_sec
+
+    def clear_lockouts(self, user_id):
+        """Clear any lockouts for the given account."""
+        for k in self.redis.keys('auth.failures:*:%s' % user_id):
+            self.redis.delete(k)
+    
+    def get_lockouts(self, user_id):
+        lockouts = []
+        for k in self.redis.keys('auth.failures:*:%s' % user_id):
+            k = k.decode('utf-8')
+            ip = k.split(':')[1]
+            failures = int(self.redis.get(k) or 0)
+            if failures >= self.lockout_threshold:
+                lockouts.append((ip, self.redis.ttl(k)))
+        return lockouts
+        
+    def is_locked_out(self, ip, user_id):
+        key = 'auth.failures:%s:%s' % (ip, user_id)
+        failures = int(self.redis.get(key) or 0)
+        if failures >= self.lockout_threshold:
+            return self.redis.ttl(key)
+        
+    def authentication_attempt(self, ip, user_id, success):
+        key = 'auth.failures:%s:%s' % (ip, user_id)
+        if success:
+            self.redis.delete(key)
+        else:
+            self.redis.incr(key)
+            self.redis.expire(key, self.lockout_duration_sec)
 
 
 # wrapper for bottle functions
@@ -45,63 +108,149 @@ def require_permission(name):
         return f
     return set_permission
 
+def scrypt_kdf(password, salt):
+    return scrypt.hash(password, salt, N=16384, r=8, p=1, buflen=16).hex()
 
-class BasePermissionBasedSecurityScheme(object):
+class UserStore(object):
+    '''
+    # Minimal User
+    CREATE TABLE user (
+        id INT NOT NULL AUTO_INCREMENT,
+        username VARCHAR(32) NOT NULL,
+        salt CHAR(32) NOT NULL DEFAULT '',
+        password CHAR(32) NOT NULL DEFAULT '',
+        status ENUM('enabled', 'disabled') NOT NULL DEFAULT 'enabled',
+        role VARCHAR(32) NOT NULL,
+        permissions VARCHAR(128) NOT NULL DEFAULT '',
+        
+        UNIQUE(username),
+        PRIMARY KEY (id)
+    );
+    '''
+    def __init__(self, db, kdf=scrypt_kdf):
+        self.db = db
+        self.kdf = kdf
     
-    @staticmethod
-    def get_user(username):
-        users = {
-            'herman': {'password': 'xmas', 'permissions': ['read', 'write']},
-            'max': {'password': 'cheese', 'permissions': ['read']},
-        }
-        return users.get(username)
+    def get_user_by_username(self, username):
+        return self.db.get('select * from user where username=%s', username)
+        
+    def get_user_by_id(self, user_id):
+        return self.db.get('select * from user where id=%s', user_id)
+        
+    def check_password(self, user, password):
+        return hmac.compare_digest(self.kdf(password, user.salt), user.password)
+        
+    def update_password(self, user_id, password):
+        salt = os.urandom(16).hex()
+        self.db.execute('update user set salt=%s, password=%s where id=%s', salt, self.kdf(password, salt), user_id)
+
+
+class PasswordPolicy(object):
+    def generate_password(self, length=8, alphabet='23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'):
+        rng = random.SystemRandom()
+        while True:
+            password = ''.join([rng.choice(alphabet) for x in range(length)])
+            if not self.check_password(password):
+                return password
     
-    def authorize(self, user, required_permission):
+    def check_password(self, password):
+        """Return None on success and an error string otherwise."""
+        if len(password) < 8:
+            return 'password must be 8 or more characters'
+        if not re.search(r'\d', password):
+            return 'password must contain at least 1 digit'
+        if not re.search(r'[a-zA-Z]', password):
+            return 'password must contain at least 1 alphabetic character'
+
+
+class AuthenticationException(Exception):
+    pass
+
+class XsrfTokenException(Exception):
+    pass
+    
+def permissions_check(required, granted):
+    '''Users must be granted at least one of the required permissions to pass the check.'''
+    required = set((required or '').strip().split())
+    granted = set((granted or '').strip().split())
+    return bool(required & granted)
+
+class SecurityScheme(object):
+    
+    def __init__(self, user_store, lockout_manager):
+        self.user_store = user_store
+        self.lockout_manager = lockout_manager
+    
+    def authorize(self, user, required_permissions):
         """Returns None on success and an error string otherwise."""
-        if required_permission not in user['permissions']:
-            return 'Unauthorized.'
-        return None
-
+        if user.status != 'enabled':
+            return 'Account disabled.'
+        if not required_permissions:
+            return None
+        if user.role != 'superuser' and not permissions_check(required_permissions, user.permissions):
+            return 'Insufficient privileges.'
+    
     def authenticate(self, username, password, ip=None):
-        """Returns a user dict on success and an error string otherwise."""
-        if not username:
-            return 'Authentication required.'
-        user = self.get_user(username)
+        """Returns a user dict on success and AuthenticationException otherwise."""
+        if not username: 
+            raise AuthenticationException('Authentication required.')
+        user = self.user_store.get_user_by_username(username)
         if not user:
-            return 'User "%s" does not exist.' % username
+            raise AuthenticationException('User does not exist.')
+        elif user.status != 'enabled':
+            raise AuthenticationException('Account disabled.')
+        
+        lockout = self.lockout_manager.is_locked_out(ip, user.id)
+        if lockout:
+            raise AuthenticationException('Too many authentication failures.  Account locked for %s seconds.' % lockout)
+        
+        if not self.user_store.check_password(user, password):
+            self.lockout_manager.authentication_attempt(ip, user.id, False)
+            raise AuthenticationException('Incorrect password.')
+        
+        self.lockout_manager.authentication_attempt(ip, user.id, True)
+        return user
 
-        if user.password != password:
-            return 'Incorrect password.'
-        else:
-            return user
+    def update_password(self, user_id, password):
+        self.user_store.update_password(user_id, password)
+        self.lockout_manager.clear_lockouts(user_id)
 
 
 class UserAuthPlugin(object):
     name = 'userauth'
-    api = 2
+    api = 2    
     
     def __init__(
-            self, 
-            security_scheme, 
-            session_storage, 
-            session_lifetime_sec=60*15,
+            self,
+            xsrf_secret,
+            user_store,
+            security_scheme,
+            session_storage,
+            session_lifetime_sec=60*15, # 15 minutes
             secure_cookie=False,
-            cookie_name='session',
+            cookie_name='SESSION',
+            cookie_name_xsrf='XSRF-TOKEN',
             realm_name='Private'):
+        self.xsrf_secret = xsrf_secret
+        self.user_store = user_store
         self.security_scheme = security_scheme
         self.session_storage = session_storage
         self.session_lifetime_sec = session_lifetime_sec
         self.cookie_name = cookie_name
-        self.set_cookie_options = {'expires': datetime.datetime.utcnow() + datetime.timedelta(days=90),}
+        self.cookie_name_xsrf = cookie_name_xsrf
+        self.set_cookie_options = {
+            'expires': datetime.datetime.utcnow() + datetime.timedelta(days=90),
+        }
         if secure_cookie:
             self.set_cookie_options['secure'] = True
         self.realm_name = realm_name
-    
-    def get_session_user(self):
-        session_id = bottle.request.get_cookie(self.cookie_name)
-        if session_id:
-            return self.session_storage.get_user(session_id)
-    
+
+    def generate_xsrf_token(self, session_id):
+        return hmac.new(
+            session_id.encode('utf-8'),
+            self.xsrf_secret.encode('utf-8'), 
+            hashlib.sha256).hexdigest()[:32]
+
     def get_user(self):
         # check basic auth before session auth
         username, password = bottle.request.auth or (None, None)
@@ -109,41 +258,58 @@ class UserAuthPlugin(object):
         if username is not None:
             return self.security_scheme.authenticate(username, password, bottle.request.remote_addr)
         elif session_id:
-            return self.session_storage.get_user(session_id, self.session_lifetime_sec * 1000)
-        return 'Authentication required.'
-    
+            user_id = self.session_storage.get_user_id(session_id)
+            if user_id is not None:
+                user = self.user_store.get_user_by_id(user_id)
+                if user is not None:
+                    xsrf_token_correct = self.generate_xsrf_token(session_id)
+                    xsrf_token = bottle.request.params.get('XSRF-TOKEN', bottle.request.headers.get('X-XSRF-TOKEN', ''))
+                    if not hmac.compare_digest(xsrf_token, xsrf_token_correct):
+                        raise XsrfTokenException('Invalid XSRF token.')
+                    self.session_storage.refresh(session_id, self.session_lifetime_sec)
+                    return user
+
     def apply(self, callback, route):
         args = inspect.getargspec(route.callback)[0]
         if 'user' not in args:
             return callback
 
         def wrapper(*args, **kwargs):
-            user_or_error = self.get_user()
-            if isinstance(user_or_error, dict):
-                authorization_error = self.security_scheme.authorize(user_or_error, callback.__dict__.get('permission', None))
-                if authorization_error:
-                    raise bottle.HTTPError(403, authorization_error)
-                kwargs['user'] = user_or_error
-                return callback(*args, **kwargs)
-
-            response = bottle.HTTPError(401, str(user_or_error))
-            # don't request basic auth from ajax requests
-            if not bottle.request.is_xhr:
-                response.headers['WWW-Authenticate'] = 'Basic realm="%s"' % self.realm_name
-            raise response
+            try:
+                user = self.get_user()
+                if user:
+                    permissions = callback.__dict__.get('permission', None)
+                    authorization_error = self.security_scheme.authorize(user, permissions)
+                    if authorization_error:
+                        raise bottle.HTTPError(403, authorization_error)
+                    kwargs['user'] = user
+                    return callback(*args, **kwargs)
+                else:
+                    raise AuthenticationException('Authentication required.')
+            except AuthenticationException as e:
+                response = bottle.HTTPError(401, str(e))
+                # don't request basic auth from ajax requests
+                if not bottle.request.is_xhr:
+                    response.headers['WWW-Authenticate'] = 'Basic realm="%s"' % self.realm_name
+                raise response
+            except XsrfTokenException as e:
+                raise bottle.HTTPError(403, str(e))
         
         return wrapper
 
     def session_login(self, user_id):
-        session_id = os.urandom(16).encode('hex')
-        self.session_storage.store(session_id, user_id, self.session_lifetime_sec * 1000)
-        bottle.response.set_cookie(self.cookie_name, session_id, path='/', **self.set_cookie_options)
+        session_id = os.urandom(16).hex()
+        self.session_storage.store(session_id, user_id, self.session_lifetime_sec)
+        bottle.response.set_cookie(self.cookie_name, session_id, path='/', httponly=True, **self.set_cookie_options)
+        xsrf_token = self.generate_xsrf_token(session_id)
+        bottle.response.set_cookie(self.cookie_name_xsrf, xsrf_token, path='/', **self.set_cookie_options)
         
     def session_logout(self):
         session_id = bottle.request.get_cookie(self.cookie_name)
         if session_id: 
             self.session_storage.expire(session_id)
             bottle.response.delete_cookie(self.cookie_name, path='/')
+            bottle.response.delete_cookie(self.cookie_name_xsrf, path='/')
 
     def logout_other_sessions(self, user_id):
         session_id = bottle.request.get_cookie(self.cookie_name) or ''
---
+++ tests/userauth.py
@@ -0,0 +1,203 @@
+import bottle
+import os
+import time
+from redis import StrictRedis
+from bmf.simpledb import Connection
+from bmf.bottleplugin.userauth import RedisLockoutManager, SqlSessionStorage, UserStore, SecurityScheme, UserAuthPlugin, AuthenticationException
+
+def test_session_storage(session_storage):
+    assert session_storage.get_user_id('test') is None
+    session_id = os.urandom(16).hex()
+    assert session_storage.get_user_id(session_id) is None
+    user_id = 0
+    session_storage.store(session_id, user_id, 10)
+    assert session_storage.get_user_id(session_id) == user_id
+    user_id = 1
+    session_storage.store(session_id, user_id, 10)
+    assert session_storage.get_user_id(session_id) == user_id
+    session_storage.expire_by_user(user_id+1)
+    assert session_storage.get_user_id(session_id) == user_id
+    session_storage.expire_by_user(user_id, session_id)
+    assert session_storage.get_user_id(session_id) == user_id
+    session_storage.expire(session_id)
+    assert session_storage.get_user_id(session_id) is None
+    user_id = 2
+    session_id = os.urandom(16).hex()
+    session_storage.store(session_id, user_id, 1)
+    assert session_storage.get_user_id(session_id) == user_id
+    time.sleep(2)
+    assert session_storage.get_user_id(session_id) is None
+    session_storage.store(session_id, user_id, 1)
+    session_storage.refresh(session_id, 3)
+    time.sleep(2)
+    assert session_storage.get_user_id(session_id) == user_id
+
+def test_lockout_manager(redis):
+    user_id = 1
+    m = RedisLockoutManager(redis, 3, 2)
+    m.clear_lockouts(user_id)
+    ip = '0.0.0.0'
+    assert not m.is_locked_out(ip, user_id)
+    m.authentication_attempt(ip, user_id, False)
+    assert not m.is_locked_out(ip, user_id)
+    m.authentication_attempt(ip, user_id, False)
+    assert not m.is_locked_out(ip, user_id)
+    m.authentication_attempt(ip, user_id, False)
+    assert m.is_locked_out(ip, user_id)
+    m.clear_lockouts(user_id)
+    assert not m.is_locked_out(ip, user_id)
+    for i in range(10):
+        m.authentication_attempt(ip, user_id, False)
+    assert m.is_locked_out(ip, user_id)
+    m.authentication_attempt(ip, user_id, True)
+    assert not m.is_locked_out(ip, user_id)
+    for i in range(10):
+        m.authentication_attempt(ip, user_id, False)
+    ip2 = '1.1.1.1'
+    for i in range(10):
+        m.authentication_attempt(ip2, user_id, False)
+    assert len(m.get_lockouts(user_id)) == 2
+    m.authentication_attempt(ip, user_id, True)
+    assert len(m.get_lockouts(user_id)) == 1
+    time.sleep(3)
+    assert len(m.get_lockouts(user_id)) == 0
+
+def test_user_store(db):
+    username = 'test2'
+    db.execute('delete from user where username=%s', username)
+    user_id = db.replace('user', username=username, role='reader')
+    user_store = UserStore(db)
+    user = user_store.get_user_by_username(username)
+    assert user.id == user_id
+    user = user_store.get_user_by_username(username.upper())
+    assert user.id == user_id
+    user = user_store.get_user_by_username(username.lower())
+    assert user.id == user_id
+    user = user_store.get_user_by_id(user_id)
+    assert user.id == user_id
+    # get users that do not exist
+    user_store.update_password(user.id, 'foo')
+    assert not user_store.check_password(user, 'bar')
+    user = user_store.get_user_by_id(user_id)
+    assert user_store.check_password(user, 'foo')
+    user = user_store.get_user_by_username('no such user')
+    assert user is None
+    user = user_store.get_user_by_username(-1)
+    assert user is None
+
+def test_security_scheme(redis, db):
+    
+    user_store = UserStore(db)
+    lockout_manager = RedisLockoutManager(redis, 3, 2)
+    scheme = SecurityScheme(user_store, lockout_manager)
+    
+    def test_auth(username, password, ip):
+        try:
+            return scheme.authenticate(username, password, ip)
+        except AuthenticationException as e:
+            return str(e)
+    
+    username = 'test1'
+    password = 'foo'
+    ip = '0.0.0.0'
+    db.execute('delete from user where username=%s', username)
+    user_id = db.replace('user', username=username, role='reader')
+    
+    user = test_auth(None, password, ip)
+    assert user == 'Authentication required.'
+    user = test_auth('no such user', password, ip)
+    assert user == 'User does not exist.'
+    db.update('user', 'username', username, status='disabled')
+    user = test_auth(username, password, ip)
+    assert user == 'Account disabled.'
+    db.update('user', 'username', username, status='enabled')
+    scheme.update_password(user_id, password)
+    user = test_auth(username, password, ip)
+    assert user.id == user_id
+    user = test_auth(username, password + 'wrong', ip)
+    assert 'Incorrect password.' == user
+    for i in range(10):
+        user = test_auth(username, password + 'wrong', ip)
+    assert 'Too many authentication failures' in user
+    user = test_auth(username, password + 'wrong', ip)
+    time.sleep(3)
+    user = test_auth(username, password, ip)
+    assert user.id == user_id
+    
+    # test disabled users
+    assert scheme.authorize(user, None) is None
+    db.update('user', 'username', username, status='disabled')
+    user = user_store.get_user_by_id(user_id)
+    assert scheme.authorize(user, None) == 'Account disabled.'
+    db.update('user', 'username', username, status='enabled')
+    user = user_store.get_user_by_id(user_id)
+    assert not scheme.authorize(user, None)
+    user.permissions = 'read write'
+    assert scheme.authorize(user, 'read') is None
+    assert scheme.authorize(user, 'write') is None
+    assert scheme.authorize(user, 'read write') is None
+    assert scheme.authorize(user, 'delete') == 'Insufficient privileges.'
+    assert scheme.authorize(user, 'delete write') is None
+    user.role = 'superuser'
+    assert scheme.authorize(user, 'delete') is None
+    user.role = 'peon'
+    assert scheme.authorize(user, 'delete')
+
+def test_plugin(redis, db):
+    
+    session_storage = SqlSessionStorage(db)
+    user_store = UserStore(db)
+    lockout_manager = RedisLockoutManager(redis, 3, 2)
+    security_scheme = SecurityScheme(user_store, lockout_manager)
+    user_id = user_store.get_user_by_username('test1').id
+    security_scheme.update_password(user_id, '12345')
+    
+    plugin = UserAuthPlugin('secret', user_store, security_scheme, session_storage)
+    
+    app = bottle.Bottle()
+    app.install(plugin)
+
+    @app.get('/hello/<name>')
+    def index(name):
+        return 'hello' + name
+        
+    @app.get('/auth/<foo>')
+    def auth(user, foo):
+        return repr(user)
+        
+    import traceback
+    
+    @app.get('/logout')
+    def logout():
+        plugin.session_logout()
+        return 'out'
+        
+    @app.get('/logout2')
+    def logout2(user):
+        plugin.logout_other_sessions(user_id)
+        return 'out'
+        
+    @app.get('/login/<username>/<password>')
+    def login(username, password):
+        try:
+            user = security_scheme.authenticate(username, password, bottle.request.remote_addr)
+            plugin.session_login(user.id)
+            return repr(user)
+        except Exception as e:
+            traceback.print_exc()
+            return repr(e)
+        #return repr(user)
+    
+    app.run(host='localhost', port=8080)
+
+
+if __name__ == "__main__":
+    
+    redis = StrictRedis()
+    db = Connection('localhost', 'test')
+    
+    #test_session_storage(session_storage)
+    #test_lockout_manager(redis)
+    #test_user_store(db)
+    #test_security_scheme(redis, db)
+    test_plugin(redis, db)
